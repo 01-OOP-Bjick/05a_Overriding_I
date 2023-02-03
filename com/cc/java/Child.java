92506d438b126cff946d9ef43d3efea0cad0a248
bjick84 <bjick@web.de>
2023-02-03 12:25:12 +0100

    Serielle Vererbung+++

--- com/cc/java/Child.java
+++ com/cc/java/Child.java
@@ -2,7 +2,7 @@ package com.cc.java;
 
 public class Child extends Parent{
 
-    public String sayHello() {
-        return "Hello from a Child!";
-    }
+    // public String sayHello() {
+    //     return "Hello from a Child!";
+    // }
 }
