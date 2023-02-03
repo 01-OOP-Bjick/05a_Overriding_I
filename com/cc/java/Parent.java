3f9ee7fdb7b8ecc90ae57a2c7447ae7573279ac4
bjick84 <bjick@web.de>
2023-02-03 12:08:13 +0100

    gleiche methoen namen overriding++

---
+++ com/cc/java/Parent.java
@@ -0,0 +1,12 @@
+package com.cc.java;
+
+public class Parent {
+
+    public String sayHello() {
+        return "Hello from a Parent!";
+    }
+    
+    public String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
+}
